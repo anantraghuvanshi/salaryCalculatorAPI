9b3fe28d9cd839ee8a6c70e964f892714f1c9451
Anant <anantsinghraghuvanshi1@gmail.com>
2023-08-16 01:28:23 +0530

    initial commit

---
+++ src/main/java/com/SalaryCalculatorAPI/SalaryCalculatorApiApplication.java
@@ -0,0 +1,13 @@
+package com.SalaryCalculatorAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalaryCalculatorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalaryCalculatorApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SalaryCalculatorAPI/SalaryCalculatorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SalaryCalculatorAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalaryCalculatorApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

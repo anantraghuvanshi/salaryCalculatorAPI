67c0c13afd137f2b478dfbe175e7d44b9a2c797e
Anant <anantsinghraghuvanshi1@gmail.com>
2023-08-16 14:02:48 +0530

    configuration configured

--- src/main/java/com/SalaryCalculatorAPI/controller/SalaryCalculatorController.java
+++ src/main/java/com/SalaryCalculatorAPI/controller/SalaryCalculatorController.java
@@ -1,7 +1,12 @@
 package com.SalaryCalculatorAPI.controller;
 
+import com.SalaryCalculatorAPI.model.SalaryRequest;
+import com.SalaryCalculatorAPI.model.SalaryResponse;
 import com.SalaryCalculatorAPI.service.SalaryCalculatorService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +16,8 @@ public class SalaryCalculatorController {
 
     @Autowired
     private SalaryCalculatorService salaryCalculatorService;
+    @PostMapping("/calculate")
+    public ResponseEntity<SalaryResponse> calculate(@RequestBody SalaryRequest request) {
+        return ResponseEntity.ok(salaryCalculatorService.calculateSalary(request));
+    }
 }
